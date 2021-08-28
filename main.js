fda92cbe6f08af113612e0e90c7a92ad70cd5f11
thisnew <87805656+CKThisNew@users.noreply.github.com>
2021-08-28 23:05:01 +0900

    nav js

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//넵바가 올라가면 투명 내라가면 배경색
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
